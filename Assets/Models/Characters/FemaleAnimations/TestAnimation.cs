95af69a795a33dd10aba1c4df9c8710f0869a16d
Elayoe <nicoline.larsen@webgolf.dk>
2017-03-17 13:21:55 +0100

    iPad click

--- Assets/Models/Characters/FemaleAnimations/TestAnimation.cs
+++ Assets/Models/Characters/FemaleAnimations/TestAnimation.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class TestAnimation : MonoBehaviour
 {
+	//iPad reference
+	public GameObject iPad;
+	//public GameObject iPadScreen;
 
     private Animator myAnimator;
     private float VSpeed;
     public float HSpeed;
 
     public Transform handRoot;
-    public List<Transform> tablets;
+	public List<Transform> tablets;
 
     public SteamVR_ControllerManager controllerMan;
+	/**
+	private SteamVR_TrackedObject trackedObj;
+
+	private SteamVR_Controller.Device controller {
+
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
+
+		}
+
+	private Valve.VR.EVRButtonId clickedButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	public bool clickedDown = false;
+	*/
+
+	private Material[] materials;
+	private int currentMat = 0;
+	private Transform ipadScreen;
+
+	/*
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}*/
+
     // Use this for initialization
     void Start ()
 	{
+
+		//iPad = GameObject.Find("iPad");
+
+		//Instantiate iPad screen color
+		ipadScreen = iPad.gameObject.transform.GetChild (0);
+		ipadScreen.GetComponent<Renderer> ().sharedMaterial.color = Color.white;
+
+		materials = new Material[2];
+		Debug.Log ("Array materials length: " + materials.Length);
+		materials [0] = (Material)Resources.Load ("White", typeof(Material)) as Material;
+		materials [1] = (Material)Resources.Load ("Test", typeof(Material))  as Material;
+
 	    myAnimator = GetComponent<Animator>();
 	    myAnimator.applyRootMotion = true;
         if (controllerTest)
@@ -37,12 +76,13 @@ public class TestAnimation : MonoBehaviour
 
     private bool sitting = false;
     private bool samba = false;
-    public bool controllerTest = false;
+    public bool controllerTest = true;
 
     Vector3 tabPos = new Vector3(0.024f, 0.523f, 0.071f);
     Vector3 tabRot = new Vector3(80, -115, -287);
     public IEnumerator GrabTablet()
     {
+		Debug.Log ("Grabbing tablet");
         myAnimator.SetBool("GrabTablet", true);
         yield return new WaitForSeconds(1.5f);
         if (controllerTest == true)
@@ -54,12 +94,12 @@ public class TestAnimation : MonoBehaviour
 
             int time = 100;
         while (time > 0)
-        {
+        	{
             controllerMan.left.GetComponent<Vibrator>().vibrate(1000);
             controllerMan.right.GetComponent<Vibrator>().vibrate(1000);
             time--;
             yield return new WaitForSeconds(0.01f);
-        }
+        	}
         }
         else
         {
@@ -77,10 +117,12 @@ public class TestAnimation : MonoBehaviour
     }
 
 
-    //public IEnumerator WalkToTarget()
-    //{
-    //    while(Vector3)
-    //}
+    public IEnumerator WalkToTarget()
+    {
+        //while(Vector3)
+
+		yield return new WaitForSeconds(0.01f);
+    }
 
     // Update is called once per frame
     void Update ()
@@ -117,5 +159,40 @@ public class TestAnimation : MonoBehaviour
         {
             StartCoroutine(GrabTablet());
         }
+
+		if (Input.GetKeyUp(KeyCode.T))
+		{
+			Debug.Log ("Changing material on tablet");
+
+
+			if (currentMat == 0) {
+				Debug.Log ("First material");
+				ipadScreen.GetComponent<Renderer> ().sharedMaterial = materials [1];
+				//ipadScreen.GetComponent<Renderer> ().sharedMaterial.color = Color.blue;
+				currentMat = 1;
+			} else {
+				Debug.Log ("Second material");
+				ipadScreen.GetComponent<Renderer> ().sharedMaterial = materials [0];
+				//ipadScreen.GetComponent<Renderer> ().sharedMaterial.color = Color.white;
+				currentMat = 0;
+			}
+		}
+
+
+		//Clicking iPad
+		/*
+		clickedDown = controller.GetPressDown(clickedButton);
+
+		if (clickedDown) {
+
+			Debug.Log("Controller clicked");
+			//public Renderer rend;
+			//start:
+			//rend = GetComponent<Renderer>()
+			//rend.enabled = true;
+			//update:
+			//rend.sharedMaterial = materials[index];
+		}
+		*/
     }
 }
