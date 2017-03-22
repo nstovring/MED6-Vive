b6162f468b34d684c7fb1eb0bb85d08d651a783c
Aishah Hussain <aishahhussain@h137.aau1x-2.klient.sydhavn.site.aau.dk>
2017-03-22 15:11:46 +0100

    Started on the sound class

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+	AudioClip ambientSound = Resources.Load<AudioClip>("Sounds/External_Hope");
+	AudioClip narrative = Resources.Load<AudioClip>("Sounds/Correct");
+	//AudioClip correctColour = Resources.Load<AudioClip>("Sounds/External_Hope");
+	//AudioClip incorrectColour = Resources.Load<AudioClip>("Sounds/External_Hope");
+
+
+	public GameObject ambient; 
+
+	public void SwitchSound (float sound) {
+		
+	switch (sound) {
+	case 0:				
+	case 1:
+	break;
+
+	}
+}
+}
