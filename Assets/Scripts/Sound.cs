3eb135e9de7d7ed35c6524f6688e43ddfec090b6
aishahhussain <aishahhussain@h137.aau1x-2.klient.sydhavn.site.aau.dk>
2017-03-27 09:50:47 +0200

    adding sound on object

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -3,41 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sound : MonoBehaviour {
-	/*public AudioClip[] ambientSounds = new AudioClip[1];
+	public AudioClip[] ambientSounds = new AudioClip[1];
 	public AudioClip[] narrationResponse = new AudioClip[2];
 	public AudioClip[] narrationQuestion = new AudioClip[1];
 	public AudioClip [] ipadSounds = new AudioClip[2];
 	public GameObject narrationSounds;
+	public GameObject ambientJingle;
 	public bool playOnAwake = false; 
 
-	void narrationSoundsResponse(){
+	void Update () {
+		//narration Sounds question
+		if (Input.GetKeyDown (KeyCode.C)) {
+			AudioSource audio = narrationSounds.GetComponent<AudioSource> ();
+			audio.clip = narrationQuestion [1];
+			audio.Play ();
+			Debug.Log ("C Pressed");
+		}
+
+		//narration sounds response
 		if (Input.GetKeyDown (KeyCode.A)) {
-			AudioSource audio = GetComponent<AudioSource> ();
+			AudioSource audio = narrationSounds.GetComponent<AudioSource> ();
 			audio.clip = narrationResponse [0];
 			audio.Play ();
 			Debug.Log ("a Pressed");
 		}
 		if (Input.GetKeyDown (KeyCode.B)) {
-			AudioSource audio = GetComponent<AudioSource> ();
+			AudioSource audio = narrationSounds.GetComponent<AudioSource> ();
 			audio.clip = narrationResponse [1];
 			audio.Play ();
 			Debug.Log ("B Pressed");
 		}
+
 	}
 
-	void narrationSoundsQuestion(){
-		if (Input.GetKeyDown (KeyCode.C)) {
-			AudioSource audio = GetComponent<AudioSource> ();
-			audio.clip = narrationQuestion [1];
-			audio.Play ();
-			Debug.Log ("C Pressed");
-		}
-	}
 	void ambient(){
-		AudioSource audio = GetComponent<AudioSource> ();
+		AudioSource audio = ambientJingle.GetComponent<AudioSource> ();
 		audio.clip = ambientSounds [0];
 		audio.Play ();
 		playOnAwake = true;
-	}*/
-
+	}
 }
+
