a7da893c214f75e2e4d388120e67304431202810
Elayoe <nicoline.larsen@webgolf.dk>
2017-04-06 14:44:57 +0200

    Asynchronous balloons

--- Assets/BalloonMovement.cs
+++ Assets/BalloonMovement.cs
@@ -6,32 +6,42 @@ public class BalloonMovement : MonoBehaviour {
 
 	private bool movingUp = true;
 	private int counter = 0;
+	public float waitSec;
 
 	// Use this for initialization
 	void Start () {
-		//new WaitForSeconds (Random.value);
+		new WaitForSeconds (5);
+		Debug.Log ("Test");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (movingUp) {
-			//Debug.Log ("Object transform before movement: " + this.gameObject.transform.position);
-			this.gameObject.transform.position += new Vector3 (0f, 0.005f, 0f);
-			counter++;
-			//Debug.Log ("Object transform after movement: " + this.gameObject.transform.position);
-			if (counter == 50) {
-				movingUp = !movingUp;
-				counter = 0;
-			}
+
+		if (waitSec >= 0) {
+			waitSec -= Time.deltaTime;
+			return;
 		} else {
-			//Debug.Log ("Object transform before movement: " + this.gameObject.transform.position);
-			this.gameObject.transform.position += new Vector3 (0f, -0.005f, 0f);
-			counter++;
-			//Debug.Log ("Object transform after movement: " + this.gameObject.transform.position);
-			if (counter == 50) {
-				movingUp = !movingUp;
-				counter = 0;
+			if (movingUp) {
+				//Debug.Log ("Object transform before movement: " + this.gameObject.transform.position);
+				this.gameObject.transform.position += new Vector3 (0f, 0.005f, 0f);
+				counter++;
+				//Debug.Log ("Object transform after movement: " + this.gameObject.transform.position);
+				if (counter == 50) {
+					movingUp = !movingUp;
+					counter = 0;
+				}
+			} else {
+				//Debug.Log ("Object transform before movement: " + this.gameObject.transform.position);
+				this.gameObject.transform.position += new Vector3 (0f, -0.005f, 0f);
+				counter++;
+				//Debug.Log ("Object transform after movement: " + this.gameObject.transform.position);
+				if (counter == 50) {
+					movingUp = !movingUp;
+					counter = 0;
+				}
 			}
 		}
+
+
 	}
 }
