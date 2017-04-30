198dc306238e178e9b5f076ff6103f84435eaa42
Nikolaj Støvring <nstovring@live.dk>
2017-04-30 17:28:49 +0200

    Commit?

---
+++ Assets/SurfaceAudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceAudioPlayer : MonoBehaviour {
+
+    AudioSource aSource;
+	// Use this for initialization
+	void Start () {
+        aSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionStay(Collider other)
+    {
+
+    }
+}
