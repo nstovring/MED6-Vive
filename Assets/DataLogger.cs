b466cc885213444fa35195493a86ed3944ce1284
Nikolaj Støvring <nstovring@live.dk>
2017-05-01 13:36:40 +0200

    Data Logger

---
+++ Assets/DataLogger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class DataLogger : MonoBehaviour {
+
+    public List<SurfaceAudioPlayer> cubes;
+
+    string fileName = "MyFile.txt";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    string loggedData;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            LogData();
+        }
+	}
+
+    void LogData()
+    {
+
+        if (File.Exists(fileName))
+        {
+            Debug.Log(fileName + " already exists.");
+            return;
+        }
+        var sr = File.CreateText(fileName);
+        sr.WriteLine("This is my file.");
+
+        foreach (var item in cubes)
+        {
+            sr.WriteLine(item.GetAccuracy());
+        }
+        
+        sr.Close();
+
+
+       
+    }
+}
--- Assets/SurfaceAudioPlayer.cs
+++ Assets/SurfaceAudioPlayer.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class SurfaceAudioPlayer : MonoBehaviour {
 
-    AudioSource aSource;
+    public List<AudioSource> aSource;
+
+    public List<AudioMixerSnapshot> snapshots;
+
+    float changeThreshold = 0.5f;
+
+    int currentSound = 0;
 	// Use this for initialization
 	void Start () {
-        aSource = GetComponent<AudioSource>();
+        changeThreshold = Random.Range(-1, 1);
 	}
+
+    public string GetAccuracy()
+    {
+        return "Threshold :" + changeThreshold + " User Selected Pos" + transform.position.z;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if(transform.localPosition.z > changeThreshold)
+        {
+            snapshots[1].TransitionTo(0.1f);
+            //aSource[1].volume = Mathf.Lerp(aSource[currentSound].volume, 1, fingerRb.velocity.sqrMagnitude / 10);
+            //aSource[0].volume = Mathf.Lerp(aSource[currentSound].volume, 0, fingerRb.velocity.sqrMagnitude / 10);
+        }
+        else
+        {
+            snapshots[0].TransitionTo(0.1f);
+        }
+    }
     Rigidbody fingerRb;
     void OnCollisionStay(Collision other)
     {
@@ -25,12 +45,15 @@ public class SurfaceAudioPlayer : MonoBehaviour {
 
         if(fingerRb != null)
         {
-            aSource.volume = Mathf.Lerp(aSource.volume,1,fingerRb.velocity.sqrMagnitude/10);
+            transform.position = new Vector3(transform.position.x, transform.position.y, fingerRb.position.z); 
+            aSource[0].volume = Mathf.Lerp(aSource[0].volume,1,fingerRb.velocity.sqrMagnitude / 10);
+            aSource[1].volume = Mathf.Lerp(aSource[1].volume, 1, fingerRb.velocity.sqrMagnitude / 10);
         }
     }
     void OnCollisionExit(Collision other)
     {
-        aSource.volume = 0;
+        aSource[0].volume = 0;
+        aSource[1].volume = 0;
         fingerRb = null;
     }
 
